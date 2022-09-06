ee6a32ead886257761cb712cd0713099e3362a6d
Wilson Ibekason <wanbekgithub@gmail.com>
2022-09-06 11:25:21 +0100

    complete intialise context structure

---
+++ src/components/TabletSidebar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Sidebar } from "primereact/sidebar";
+import { useGlobalContext } from "../services/OnGlobalContext";
+
+const TabletSidebar = () => {
+  const { GiHamburgerMenu } = useGlobalContext();
+  return (
+    <>
+      <div>fkkffkk</div>
+    </>
+  );
+};
+
+export default TabletSidebar;
