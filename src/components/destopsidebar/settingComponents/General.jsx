8e8769c023e2e06276390c4f5ea9a8a4eb95dc5b
Wilson Ibekason <wanbekgithub@gmail.com>
2022-09-09 00:39:12 +0100

    complete add tab structure UI

--- src/components/destopsidebar/settingComponents/General.jsx
+++ src/components/destopsidebar/settingComponents/General.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const General = () => {
-  return <div>General</div>;
+  return <div className="text-black text-5xl">General</div>;
 };
 
 export default General;
--- src/components/projectsetting/Layout.jsx
+++ src/components/projectsetting/Layout.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { AiOutlineQuestion } from "react-icons/ai";
+import { useNavigate, NavLink } from "react-router-dom";
+import { useGlobalContext } from "../../services/OnGlobalContext";
 import styles from "../../styles/firebaseCustomStyles";
 import { tabElements } from "../../utils/data";
 import DesktopSidebar from "../DesktopSidebar";
@@ -7,6 +9,7 @@ import Navbar from "../Navbar";
 import TabletSidebar from "../TabletSidebar";
 
 const Layout = ({ children }) => {
+  const { AiOutlineArrowRight, AiOutlineArrowLeft } = useGlobalContext();
   const navigate = useNavigate();
   const { header } = styles;
   return (
@@ -17,35 +20,53 @@ const Layout = ({ children }) => {
           <TabletSidebar />
         </div>
         <div className="min-h-[30vh] flex-1">
-          <div className=" bg-gray-100 min-h-[50vh] p-3   ">
+          <div className=" bg-gray-100 min-h-[100vh] p-3">
             {/* header */}
             <div className="">
               <Navbar />
             </div>
             {/* navbar container */}
             {/* tabs elememt */}
-            <div className="flex flex-start mb-3">
-              <h4 className={`${header} capitalize`}>project settings</h4>
+            <div className="px-4 mt-4 md:mt-4 lg:mt-2">
+              <div className="flex flex-row justify-between mb-3">
+                <h4 className={`${header} capitalize`}>project settings</h4>
+                <div className="py-[2px] px-[10px] rounded-full justify-center items-center flex bg-gray-600">
+                  <AiOutlineQuestion size={14} />
+                </div>
+              </div>
+              {/* tablte scrollbar */}
+              <div className="relative text-sm font-medium text-center text-gray-500 border-b border-gray-200 ">
+                <div className="absolute phone:hidden desktop:hidden Xdesktop:hidden tablet:block left-0 top-2">
+                  {" "}
+                  <AiOutlineArrowLeft size={20} className="" />
+                </div>
+                <div className="absolute phone:hidden desktop:hidden Xdesktop:hidden tablet:block right-0 top-2">
+                  {" "}
+                  <AiOutlineArrowRight size={20} className="" />
+                </div>
+                <ul className="flex  -mb-px overflow-x-auto desktop:w-full tablet:w-[800px] desktop:flex-wrap tablet:whitespace-nowrap tablet:overflow-x-auto scrollbar-none space-x-2">
+                  {/* cursor-not-allowed */}
+                  {tabElements.map((item, index) => {
+                    const { id, link, name, customStyles } = item;
+                    return (
+                      <li
+                        key={id}
+                        className={`${customStyles} `}
+                        //   onClick={() => navigate(`${link}`, { replace: true })}
+                      >
+                        <NavLink
+                          className=" text-gray-700 inline-block py-2 px-4 rounded-t-sm border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300 capitalize font-raleway font-semibold hover:bg-gray-300"
+                          to={link}
+                        >
+                          {name}
+                        </NavLink>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
             </div>
-            <div class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
-              <ul class="flex flex-wrap -mb-px">
-                {/* cursor-not-allowed */}
-                {tabElements.map((item, index) => {
-                  const { id, link, name, customStyles } = item;
-                  return (
-                    <li
-                      key={id}
-                      className={`${customStyles}`}
-                      onClick={() => navigate(`${link}`, { replace: true })}
-                    >
-                      <a class=" text-gray-400 dark:text-gray-500 inline-block p-4 rounded-t-lg border-b-2 border-transparent hover:text-gray-600 hover:border-gray-300">
-                        {name}
-                      </a>
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
+            {/* end of tab element */}
             {children}
           </div>
         </div>
