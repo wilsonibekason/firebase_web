4d4d8d475c7db6f8217a1098d42a8e6b16f9f95c
Wilson Ibekason <wanbekgithub@gmail.com>
2022-09-19 16:09:51 +0100

    add scroll to top hook to the firebase application

--- src/main.jsx
+++ src/main.jsx
@@ -7,19 +7,19 @@ import { BrowserRouter as Router } from "react-router-dom";
 // import "primereact/resources/themes/lara-light-indigo/theme.css";
 // import "primereact/resources/primereact.css";
 // import "primeflex/primeflex.css";
-// import ScrollToTop from "./hooks/scrollToTop";
 import "./index.css";
 import { GlobalProvider } from "./services/OnGlobalContext";
 import { IconProvider } from "./services/OnIconContext";
+import ScrollToTop from "./hooks/ScrollToTop";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <GlobalProvider>
         <IconProvider>
-          {/* <ScrollToTop> */}
-          <App />
-          {/* </ScrollToTop> */}
+          <ScrollToTop>
+            <App />
+          </ScrollToTop>
         </IconProvider>
       </GlobalProvider>
     </Router>
